b3fae396d512a58cf6ae94399098eca6fb682edd
DEED1T <hamdilazaar.vp@gmail.com>
2021-06-15 19:24:27 +0200

    75

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,9 @@
 var map = L.map('map').setView([47, 2], 6);
 var markers = L.markerClusterGroup();
-
+var static_url = "/img/center.png"
+var icon = L.icon({
+    iconUrl: static_url
+});
 
 $( document ).ready(function() {
     console.log("ready");
@@ -54,6 +57,10 @@ function addCenters(centers) {
     map.addLayer(markers);
 }
 
+markers.on('click', function(e){
+    console.log("popup")
+})
+
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 15,
@@ -105,6 +112,51 @@ function onEachFeature(feature, layer) {
         mouseover: setHighlight,
         mouseout: resetHighlight,
         click: updateData
+    });
+}
+
+//CHART
+const dataGeo = {
+    labels: ['Vaccinés','Malades','Morts'],
+    datasets : [{
+        label: "Malade",
+        backgroundColor: ['lightgreen','lightcoral','#050101'],
+        data: [5706378,16470369,105631]
+    }]
+};
+
+const configGeo = {
+    type: 'doughnut',
+    data: dataGeo,
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: 'Statistiques Nationales'
+        },
+      },
+      responsive: true,
+      
+      }
+    
+};
+
+var chartGeo = new Chart(
+    document.getElementById('chart'),
+    configGeo
+);
+
+function updateData(e) {
+    
+    var layer = e.target;
+    var region = layer.feature.properties['code'];
+    chartGeo.options.plugins.title.text = 'Statistiques de la Région '+layer.feature.properties['nom'];
+    //chartGeo.data.datasets[0].data = stats[region];
+    chartGeo.update()
+
+
+    layer.setStyle({
+        color: '#f14b4d'
     });
 }
 
